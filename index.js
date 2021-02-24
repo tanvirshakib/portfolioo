2143ca361303cac62f274643fc0b0544403ca93f
tanvirshakib <53853637+tanvirshakib@users.noreply.github.com>
2021-02-24 23:00:43 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.toggle').click(function(){
+			$('.toggle').toggleClass('active')
+			$('nav').toggleClass('active')
+		})
+	})
+	</script>
+  
